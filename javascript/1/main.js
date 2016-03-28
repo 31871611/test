df3f094a5b46ea96280344dd823f04d2b67d4db5
31871611 <31871611@qq.com>
2016-03-28 23:34:33 +0800

    test20160328

--- javascript/1/main.js
+++ javascript/1/main.js
@@ -1,12 +1,14 @@
 require.config({
     paths:{
-        jquery:"../../public/js/jquery-1.8.3.min"
+        jquery:"../../public/js/jquery-1.8.3.min",
+        jqueryUI:"http://code.jquery.com/ui/1.10.4/jquery-ui"
     }
 });
 
 require(["jquery","window"],function($,w){
     $("#a").on("click",function(){
-        new w.Window().alert({
+        var win=new w.Window();
+        win.alert({
             title:"提示",
             content:"welcome!",
             //handler:function(){
@@ -17,6 +19,7 @@ require(["jquery","window"],function($,w){
             y:50,
             hasCloseBtn:true,
             skinClassName:"window_skin_a",
+            dragHandle:".window_header",
             text4AlertBtn:"ok",
             handler4AlertBtn:function(){
                 alert("点击了确定");
@@ -24,6 +27,13 @@ require(["jquery","window"],function($,w){
             handler4CloseBtn:function(){
                 alert("点击了关闭");
             }
-        })
+        }).on("alert",function(){
+            alert("the second alert handler");
+        }).on("alert",function(){
+            alert("the third alert handler");
+        });
+        win.on("close",function(){
+            alert("the second close handler");
+        });
     });
 });
--- javascript/1/window.js
+++ javascript/1/window.js
@@ -1,4 +1,4 @@
-define(["jquery"],function($){
+define(["jquery","jqueryUI"],function($,$UI){
     function Window(){
         this.cfg={
             width:500,
@@ -7,32 +7,63 @@ define(["jquery"],function($){
             content:"",
             handler:null,
             hasCloseBtn:false,
+            hasMask:true,               //是否显示遮罩
+            isDraggable:true,           //拖动
+            dragHandle:null,            //可拖动的区域
             skinClassName:null,         //皮肤
-            text4AlertBtn:"确定",         //按钮文字
+            text4AlertBtn:"确定",       //按钮文字
             handler4AlertBtn:null,      //给关闭按钮添加回调接口
             handler4CloseBtn:null       //给关闭按钮添加回调接口
-        }
+        };
+        this.handlers={}
     }
     Window.prototype={
+        //自定义事件
+        //观察者模式
+        //优点：跳出原生事件的限制，提高封闭的抽象层级
+        on:function(type,handler){
+            if(typeof this.handlers[type] == "undefined"){
+                this.handlers[type]=[];
+            }
+            this.handlers[type].push(handler);
+            //连缀语法
+            return this;
+        },
+        fire:function(type,data){
+            if(this.handlers[type] instanceof Array){
+                var handlers=this.handlers[type];
+                for(var i= 0,len=handlers.length;i<len;i++){
+                    handlers[i](data);
+                }
+            }
+        },
         alert:function(cfg){
             var CFG=$.extend(this.cfg,cfg);
 
             var boundingBox=$(
                 '<div class="window_boundingBox">' +
-                    '<div class="window_title">'+CFG.title+'</div>'+
+                    '<div class="window_header">'+CFG.title+'</div>'+
                     '<div class="window_body">'+CFG.content+'</div>'+
                     '<div class="window_footer"><input type="button" class="window_alertBtn" value="'+CFG.text4AlertBtn+'" /></div>'+
                 '</div>'
             );
             //btn=boundingBox.find(".window_footer input");
             btn=boundingBox.find(".window_alertBtn");
+            mask=null;
+            that=this;
+            if(CFG.hasMask){
+                mask=$('<div class="window_mask"></div>');
+                mask.append("body");
+            }
             boundingBox.appendTo("body");
             //boundingBox.html(CFG.content);
             //btn.appendTo(boundingBox);
 
             btn.click(function(){
-                CFG.handler4AlertBtn && CFG.handler4AlertBtn();
+                //CFG.handler4AlertBtn && CFG.handler4AlertBtn();
                 boundingBox.remove();
+                mask && mask.remove();
+                that.fire("alert");
             });
 
             boundingBox.css({
@@ -46,14 +77,33 @@ define(["jquery"],function($){
                 var closeBth=$('<span class="window_closeBtn">x</span>');
                 closeBth.appendTo(boundingBox);
                 closeBth.click(function(){
-                    CFG.handler4CloseBtn && CFG.handler4CloseBtn();
+                    //CFG.handler4CloseBtn && CFG.handler4CloseBtn();
                     boundingBox.remove();
+                    mask && mask.remove();
+                    that.fire("close");
                 });
+            }
+
+            if(CFG.handler4AlertBtn){
+                this.on("alert",CFG.handler4AlertBtn);
+            }
+            if(CFG.handler4CloseBtn){
+                this.on("close",CFG.handler4CloseBtn);
             }
 
             if(CFG.skinClassName){
                 boundingBox.addClass(CFG.skinClassName);
             }
+            if(CFG.isDraggable){
+                if(CFG.dragHandle){
+                    boundingBox.draggable({handle:CFG.dragHandle});
+                }else{
+                    boundingBox.draggable();
+                }
+            }
+
+            //连缀语法
+            return this;
         },
         confirm:function(){
             console.log("confirm");
