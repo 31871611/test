36c1fe049e4e5593d2da16a9d04a4da23a578c81
btname <btname@163.com>
2016-03-31 17:28:59 +0800

    test20160331

--- vue/js/test.js
+++ vue/js/test.js
@@ -1,5 +1,72 @@
 require(["vue"],function(Vue){
 
+    //用全局方法 Vue.filter() 注册一个自定义过滤器，它接收两个参数：过滤器ID和过滤器函数。过滤器函数以值为参数，返回转换后的值
+    Vue.filter("split0",function(value){
+        return value.split(".")[0];
+    });
+    //过滤器函数可以接收任意数量的参数：
+    Vue.filter("wrap",function(value,begin,end){
+        return begin + value + end;
+    });
+    //把来自视图（< input > 元素）的值写回模型之前转化它
+    Vue.filter("currencyDisplay",{
+        // model -> view
+        // 在更新 `<input>` 元素之前格式化值
+        read:function(val){
+            return '￥'+val.toFixed(2);
+        },
+        // view -> model
+        // 在写回数据之前格式化值
+        write:function(val,oldVal){
+            var number=+val.replace(/[^\d.]/g,'');
+            return isNaN(number) ? 0 :parseFloat(number.toFixed(2));
+        }
+    });
+    //
+    Vue.filter("concat",function(value,input){
+        //这里input===this.userInput
+        return value+input;
+    });
+
+
+    //过渡动画
+    Vue.transition("expand",{
+        beforeEnter:function (el){
+            el.textContent="beforEnter";
+        },
+        enter:function(el){
+            el.textContent="enter";
+        },
+        afterEnter:function(el){
+            el.textContent="afterEnter";
+        },
+        enterCancelled:function(el){
+
+        },
+        beforeLeave:function(el){
+            el.textContent="beforeLeave";
+        },
+        leave:function(el){
+            el.textContent="leave";
+        },
+        afterLeave:function(el){
+            el.textContent="afterLeave";
+        },
+        leaveCancelled:function(el){
+
+        }
+    });
+    Vue.transition("stagger",{
+        stagger:function(index){
+            // 每个过渡项目增加 50ms 延时
+            // 但是最大延时限制为 300ms
+            return Math.min(300, index * 50);
+        }
+    });
+
+    //用 Vue.extend() 创建一个组件构造器
+
+
     var data1={
         message:"hello vue.js",
         a:1,
@@ -36,7 +103,10 @@ require(["vue"],function(Vue){
             {text:"one",value:"a"},
             {text:"two",value:"b"},
             {text:"three",value:"c"}
-        ]
+        ],
+        userInput:1,
+        show:true,
+        query:""
     };
     var vm1=new Vue({
         el:"#app",
@@ -52,10 +122,33 @@ require(["vue"],function(Vue){
             c:function(){
                 return this.message.toString().length;
             }
+        },
+        // 在 'methods' 对象中定义方法
+        methods:{
+            greet:function(event){
+                //方法内'this'指向vm
+                alert("hello"+this.msg+"!");
+                //event是原生dom事件
+                alert(event.target.tagName);
+                //alert(this.innerText);
+                //alert(this.innerHTML);
+            },
+            say:function(val,event){
+                alert(val);
+                // 现在我们可以访问原生事件对象
+                event.preventDefault();
+            },
+            doThis:function(){},
+            onSubmit:function(){
+                alert("onSubmit");
+            },
+            doThat:function(){}
         }
     });
     //设置属性也会影响到原始数据
     data1.a=2;
-    data1.msg="修改";
+    data1.msg="修.改";
+
+
 
 });
