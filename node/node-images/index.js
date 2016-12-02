1de8ed97e1cbe278834a60982f25c6c73dc30667
31871611 <31871611@qq.com>
2016-12-03 00:29:13 +0800

    master node-images

--- node/node-images/index.js
+++ node/node-images/index.js
@@ -19,41 +19,9 @@ images("input.jpg")                     //Load image from file
     });
 
 
-
- var img=images("input.jpg").size();
-
- console.log(img.width);
- console.log(img.height);
-
     //原图大小   截图大小
     w:750 / 2 - 300 / 2 = 375 - 150 = 225
     h:500 / 2 - 300 / 2 = 250 - 150 = 100
-
-
-
-//原图大小
-var img=images("input/input.jpg").size();
-//输出大小
-var out={
-    w:500,
-    h:500
-};
-//截图xy坐标
-var pos={
-  x:img.width / 2 - out.w / 2,
-  y:img.height / 2 - out.h / 2
-};
-//原图小于截图大小?
-if(img.width < out.w && img.height < out.h){
-    console.log('原图不能小于截图大小');
-    return;
-}
-
-images(images("input/input.jpg"),pos.x,pos.y,out.w,out.h)
-    .save("out/output.jpg");
-
-
-console.log(pos);
 
  */
 
@@ -63,25 +31,30 @@ console.log(pos);
 //files 是一个存储目录中所包含的文件名称的数组，数组中不包括 '.' 和 '..'
 var fs = require('fs');
 var errorArr=[];
-fs.readdir('./input',function(err,files){
+var count=0;
+var inputFiles='./input/';
+var outFiles='./out/';
+
+fs.readdir(inputFiles,function(err,files){
     if (err) throw err;
+    console.log('本次需要处理文件数：' + files.length);
     for(var i=0;i < files.length;i++){
-        //console.log('图片' + (i+1) + '：' + files[i]);
 
         //原图大小
-        var img=images("./input/" + files[i]).size();
+        var img=images(inputFiles + files[i]).size();
         //输出大小
         var out={
             w:1000,
             h:1000
         };
-        //截图xy坐标
+
+        /*
+
+        //以图片中心点截xy坐标
         var pos={
             x:img.width / 2 - out.w / 2,
             y:img.height / 2 - out.h / 2
         };
-
-        /*
 
         //原图小于截图大小?
         if(img.width < out.w && img.height < out.h){
@@ -93,26 +66,29 @@ fs.readdir('./input',function(err,files){
             continue;
         }
 
-
         //按相关参数截图
-        images(images("./input/" + files[i]),pos.x,pos.y,out.w,out.h)
-            .save("./out/" + files[i]);
+        images(images(inputFiles + files[i]),pos.x,pos.y,out.w,out.h)
+            .save(outFiles + files[i]);
 
         */
 
-        images(images("./input/" + files[i]))
+        images(images(inputFiles + files[i]))
             //等比缩放图像到400像素宽
             .size(out.w,out.h)
-            .save("./out/" + files[i],{
+            .save(outFiles + files[i],{
                 quality : 50                    //保存图片到文件,图片质量为50
             });
 
+        //记录成功数.怎么算成功？怎么算失败？
+        count++;
 
     }
 
     if(errorArr.length > 1){
         console.log('以下文件不能小于'+ out.w + 'x' + out.h +'\r\n'  + errorArr);
     }
+
+    //console.log('本次成功处理文件数：' + count);
 
 });
 
