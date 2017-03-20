f68ac91ddc11a648eed1667ceb8eb2242ed94777
btname <btname@163.com>
2017-03-20 14:42:08 +0800

    商品详情页split组件样式

---
+++ vue/vue1/src/components/split/split.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="split">{{msg}}</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data () {
+    return {
+      msg: 'split'
+    }
+  },
+  ready: function () {
+
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+
+}
+</script>
+<style lang="scss" src="../../common/scss/_split.scss"></style>
+
