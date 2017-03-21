41975e4e149fe4af5240a4a6f6080ffa0aa70c09
btname <btname@163.com>
2017-03-21 14:26:58 +0800

    评论

--- vue/vue1/src/components/ratings/ratings.vue
+++ vue/vue1/src/components/ratings/ratings.vue
@@ -1,23 +1,139 @@
-<style>
-
-</style>
-
 <template>
-    <h1>{{msg}}</h1>
+  <div class="ratings" v-el:ratings>
+    <div class="ratings-content">
+      <div class="overview">
+        <div class="overview-left">
+          <h1 class="score">{{seller.score}}</h1>
+          <div class="title">综合评分</div>
+          <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+        </div>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <star :size="36" :score="seller.serviceScore"></star>
+            <span class="score">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wrapper">
+            <span class="title">商品评分</span>
+            <star :size="36" :score="seller.foodScore"></star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
+          </div>
+        </div>
+      </div>
+
+      <split></split>
+      <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="ratings"></ratingselect>
+
+      <div class="rating-wrapper">
+        <ul>
+          <li class="rating-item" v-show="needShow(rating.rateType, rating.text)" v-for="rating in ratings">
+            <div class="avatar">
+              <img width="28" height="28" :src="rating.avatar">
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.username}}</h1>
+              <div class="star-wrapper">
+                <star :size="24" :score="rating.score"></star>
+                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}</span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                <span class="icon-thumb_up"></span>
+                <span class="item" v-for="item in rating.recommend">{{item}}</span>
+              </div>
+              <div class="time">
+                {{rating.rateTime | formatDate}}
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-    export default {
-        data () {
-            return {
-                msg: 'ratings'
-            }
-        },
-        ready: function () {
+import BScroll from 'better-scroll';
+import {formatDate} from '../../common/js/date'
+import split from '../split/split'
+import star from '../star/star'
+import ratingselect from 'components/ratingselect/ratingselect';
 
-        },
-        methods: {},
-        components: {}
+const ALL = 2;
+const ERR_OK = 0;
 
+export default {
+  props:{
+    seller: {
+      type: Object
     }
+  },
+  data () {
+    return {
+      ratings:[],
+      selectType: ALL,
+      onlyContent: true
+    }
+  },
+  ready: function () {
+
+  },
+  created() {
+    this.$http.get('/api/ratings').then((response) => {
+      let res = response.data;
+      if (res.error === ERR_OK) {
+        this.ratings = res.data;
+        this.$nextTick(() => {
+          this.scroll = new BScroll(this.$els.ratings, {
+            click: true
+          });
+        });
+      }
+    });
+  },
+  methods: {
+    needShow(type, text) {
+      if (this.onlyContent && !text) {
+        return false;
+      }
+      if (this.selectType === ALL) {
+        return true;
+      } else {
+        return type === this.selectType;
+      }
+    }
+  },
+  events: {
+    'ratingtype.select'(type) {
+      this.selectType = type;
+      this.$nextTick(() => {
+        this.scroll.refresh();
+      });
+    },
+    'content.toggle'(onlyContent) {
+      this.onlyContent = onlyContent;
+      this.$nextTick(() => {
+        this.scroll.refresh();
+      });
+    }
+  },
+  filters: {
+    formatDate(time) {
+      let date = new Date(time);
+      return formatDate(date, 'yyyy-MM-dd hh:mm');
+    }
+  },
+  components: {
+    split,
+    star,
+    ratingselect
+  }
+
+}
 </script>
+<style lang="scss" src="../../common/scss/ratings.scss"></style>
