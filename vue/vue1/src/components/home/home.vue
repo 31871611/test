5e3ad5e172ffb03f02a722527bbf28d475dfde5f
btname <btname@163.com>
2017-03-31 11:13:25 +0800

    修改样式

--- vue/vue1/src/components/home/home.vue
+++ vue/vue1/src/components/home/home.vue
@@ -1,8 +1,16 @@
 <template>
-  <ul>
-    <li><a v-link="'/a?id=1&type=1'">测试路由跳转</a></li>
-    <li><a v-link="'/ele/goods'">饿了吗</a></li>
-  </ul>
+  <div class="wrap">
+    <header>header</header>
+    <article class="main">
+      <div class="mainScroll">
+        <ul>
+          <li><a v-link="'/a?id=1&type=1'">测试路由跳转</a></li>
+          <li><a v-link="'/ele/goods'">饿了吗</a></li>
+        </ul>
+      </div>
+    </article>
+    <footer>footer</footer>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
