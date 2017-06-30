48cc3ca5443199abd6a0dcf575cc7cca8196679a
btname <btname@163.com>
2017-06-30 16:16:57 +0800

    vuex

--- vue/vue2/src/components/vuex/vuex.vue
+++ vue/vue2/src/components/vuex/vuex.vue
@@ -2,7 +2,8 @@
   <div class="vuex">
     {{msg}}
     <p>{{$store.state.user_name}}</p>
-    <div @click="update">变更</div>
+    <div @click="update()">变更</div>
+    <div @click="action()">action方法</div>
     <div>{{doneTodosCount}}</div>
   </div>
 </template>
@@ -27,9 +28,22 @@ export default {
   methods:{
     update:function(){
       // 通过 store.commit 方法触发状态变更
+      // mutations中showUserName事件
       this.$store.commit('showUserName')
 
+      // 变更count值
+      this.$store.commit('increment',10)
+
       console.log(this.$store.state.user_name);
+
+      // 重新读取count值
+      console.log(this.$store.state.count);
+    },
+    action:function(){
+
+      // Action 通过 store.dispatch 方法触发
+      this.$store.dispatch('increment')
+
     }
   },
   computed: {
--- vue/vue2/src/store/index.js
+++ vue/vue2/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { SOME_MUTATION } from './mutation-types'
 
 Vue.use(Vuex);
 
@@ -7,6 +8,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   // 共享状态state
   state:{
+    count: 1,
     user_name:"store用户名",
     todos: [
       { id: 1, text: '...', done: true },
@@ -24,8 +26,25 @@ export default new Vuex.Store({
   },
   // 改变状态的方法
   mutations:{
+    increment (state,n) {
+      // 变更状态
+      state.count += n
+    },
     showUserName(state){
       alert(state.user_name);
+    },
+    // 读取mutation-types.js中值
+    [SOME_MUTATION](){
+
+    }
+  },
+  // Action 类似于 mutation，不同在于：
+  // Action 提交的是 mutation，而不是直接变更状态。
+  // Action 可以包含任意异步操作。
+  // mutation 必须同步执行这个限制么？Action 就不受约束！
+  actions: {
+    increment (context) {
+      context.commit('increment',2)
     }
   }
 })
