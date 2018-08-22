94b92b30b38a91d18a101bc51c10799bd246c71a
btname <btname@163.com>
2018-08-22 17:56:28 +0800

    模版

---
+++ vue/vue2/src/components/tpl.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="tpl">
+    {{msg}}
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'tpl',
+  data () {
+    return {
+      msg: 'tpl'
+    }
+  },
+  beforeCreate: function () { // 组件实例被创建，组件属性计算之前，如data属性等
+
+  },
+  created: function () {  // 组件实例创建完成，属性已绑定，但DOM还未生成，$el属性还不存在
+
+  },
+  beforeMount: function () {  // 模板编译/挂载之前
+
+  },
+  mounted: function () {  // 模板编译/挂载之后（不保证组件已在document中）
+
+  },
+  beforeUpdate: function () { // 组件更新之前
+
+  },
+  updated: function () { // 组件更新之后
+
+  },
+  beforeDestroy: function () { // 组件销毁前调用
+
+  },
+  destroyed: function () { // 组件销毁后调用
+
+  }
+}
+</script>
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- vue/vue2/src/router/index.js
+++ vue/vue2/src/router/index.js
@@ -12,6 +12,14 @@ const router = new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/tpl',
+      name: 'Tpl',
+      component: resolve => require(['@/components/tpl'], resolve),
+      meta:{
+        requireAuth: true,  // 添加该字段，表示进入这个路由是需要登录的
+      }
     }
   ]
 })
@@ -19,7 +27,21 @@ const router = new Router({
 // 路由跳转前的钩子
 router.beforeEach(function (to, from, next) {
   store.commit('updateLoadingStatus', {isLoading: true})
-  next()
+  if(to.meta.requireAuth){
+    alert('需要登录');
+    /*
+     if (store.state.token) {  // 通过vuex state获取当前的token是否存在
+       next();
+     }else {
+       next({
+         path: '/login',
+         query: {redirect: to.fullPath}  // 将跳转的路由path作为参数，登录成功后跳转到该路由.login?redirect=url
+       })
+     }
+     */
+  }else{
+    next()
+  }
 })
 
 // 路由跳转后的钩子
