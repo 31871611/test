33dda11ac1f04f9166b06766294578b2f48cf4e1
31871611 <31871611@qq.com>
2018-11-20 16:23:31 +0800

    图片路径

--- php/zerg/application/extra/setting.php
+++ php/zerg/application/extra/setting.php
@@ -3,6 +3,6 @@
  * 非保密信息
  */
 return [
-    'img_prefix' => 'http://localhost/zerg/public/index.php/images',
+    'img_prefix' => 'http://localhost/zerg/public/images',
     'token_expire_in' => 7200                           //缓存过期时间
 ];
