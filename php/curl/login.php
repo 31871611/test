ffb8a7fc3b8f708b3b74c255a0fc9f930f02016f
31871611 <31871611@qq.com>
2019-09-09 15:29:28 +0800

    t

--- php/curl/login.php
+++ php/curl/login.php
@@ -8,43 +8,64 @@ curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $contents = curl_exec($ch);
 curl_close($ch);
-preg_match('/<input\s*type="hidden"\s*name="formhash"\s*value="(.*?)"\s*\/>/i', $contents, $matches);
-$formhash = "";
-if(!empty($matches)) {
-    $formhash = $matches[1];
-} else {
-    die('Not found the forumhash.');
-}
-//POST数据，获取COOKIE
+preg_match('/<input\s*type="hidden"\s*name="formhash"\s*value="(.*?)"\s*\/>/i', $contents, $formhash);
+preg_match('/loginhash=(\w*)/i', $contents, $loginhash);
+
+// POST数据，获取COOKIE
 $cookie_file = dirname(__FILE__) . '/cookie.txt';
-$ch = curl_init("http://www.zuanke8.com/member.php?mod=logging&action=login&loginsubmit=yes&loginhash=LL5R5&inajax=1");
+// 登录
+$ch = curl_init("http://www.zuanke8.com/member.php?mod=logging&action=login&loginsubmit=yes&loginhash=".$loginhash[1]."&inajax=1");
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POST, 1);
 $data = array(
-    "formhash" => $formhash,
-    "referer" => "http://www.zuanke8.com/re.php",
+    "formhash" => $formhash[1],
+    "referer" => "http://www.zuanke8.com/",
     "loginfield" => "username",
     "username" => "",
     "password" => md5(""),
-    "questionid" => "4",
+    "questionid" => 4,
     "answer" => mb_convert_encoding('小白', "gbk"),
     "cookietime" => "2592000"
 );
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file);
-$res = curl_exec($ch);
+curl_exec($ch);
 curl_close($ch);
-echo ($res);
 
-/*
-//带着上面得到的COOKIE获取需要登录后才能查看的页面内容
-$ch = curl_init("http://www.zuanke8.com/thread-6358323-1-1.html");
+
+
+
+// 先打开页面，带着上面得到的COOKIE获取需要登录后才能查看的页面内容
+$ch = curl_init("http://www.zuanke8.com/home.php?mod=spacecp&ac=credit&op=buy&accept=1");
 curl_setopt($ch, CURLOPT_HEADER, 0);
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+// 将获取数据返回
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// 带上cookie
+curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file);
+$contents = curl_exec($ch);
+curl_close($ch);
+preg_match('/<input\s*type="hidden"\s*name="formhash"\s*value="(.*?)"\s*\/>/i', $contents, $formhash2);
+// 去请求
+$ch = curl_init("http://www.zuanke8.com/home.php?mod=spacecp&ac=credit&op=buy&inajax=1");
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_POST, 1);
+$data2 = array(
+    "formhash" => $formhash2[1],
+    "addfundssubmit" => "true",
+    "handlekey" => "buycredit",
+    "bank_type" => "alipay",
+    "addfundamount" => "1000"
+);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file);
 $contents = curl_exec($ch);
 curl_close($ch);
 
-var_dump($contents);
-*/
+echo $contents;
+
+//清理cookie文件
+//unlink($cookie_file);
+
