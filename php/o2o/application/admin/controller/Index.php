f0ed6a74961844a27d29e2c2028b54f73baa4377
31871611 <31871611@qq.com>
2020-03-23 11:25:50 +0800

    百度地图

--- php/o2o/application/admin/controller/Index.php
+++ php/o2o/application/admin/controller/Index.php
@@ -11,6 +11,18 @@ class Index extends Controller
         return $this->fetch();
     }
 
+    // 测试调用百度，根据地址来获取经续度
+    public function test(){
+        $res = \Map::getLngLat('北京昌平沙河地铁');
+        var_dump($res);
+    }
+
+    public function map(){
+        $res = \Map::staticimage('北京昌平沙河地铁');
+        return $res;
+        // 模版中：<img src="{:url('index/map')}" alt="">
+    }
+
     public function weblcome(){
         // return 'admin/index/weblcome';
         return '欢迎来到o2o主后台首页！';
